b0de11986f2f7fb03499a7966bf3d0aa23dc7a8c
MrThursby <smykoil@ya.ru>
2021-05-24 11:31:38 +0300

    Vk confirm logger

--- routes/bot_handler.php
+++ routes/bot_handler.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\BotHandler\WelcomeController;
 use App\Facades\BotFacade as Bot;
+use Illuminate\Support\Facades\Log;
 
 Bot::registerRoute('/bot_handler', function () {
     Bot::registerCommand('/start', [new WelcomeController, 'index']);
 
     Bot::registerCommand('vk_confirmation', function () {
+        Log::info('Vk confirm started');
         return response(env('VK_CONFIRMATION'));
     });
 });
